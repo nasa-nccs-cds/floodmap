5de8810985170947c3ad09a1b6cc5ebc5fa2d65b
Thomas Maxwell <thomas.maxwell@nasa.gov>
2020-07-16 22:57:27 -0400

    Memory op

--- floodmap/surfaceMapping/processing.py
+++ floodmap/surfaceMapping/processing.py
@@ -25,33 +25,37 @@ class LakeMaskProcessor:
         return logger
 
     def process_lakes( self, reproject_inputs, **kwargs ):
-        year_range = self._defaults['year_range']
-        return_results = kwargs.get('return_results',False)
-        lakeMaskSpecs = self._defaults.get( "lake_masks", None )
-        data_dir = lakeMaskSpecs["basedir"]
-        lake_index_range = lakeMaskSpecs["lake_index_range"]
-        directorys_spec = lakeMaskSpecs["subdir"]
-        files_spec = lakeMaskSpecs["file"]
-        lake_masks = {}
-        lake_indices = []
-        for year in range( int(year_range[0]), int(year_range[1]) + 1 ):
-            year_dir = os.path.join( data_dir, directorys_spec.format( year=year ) )
-            _lake_indices = lake_indices if year > year_range[0] else range(lake_index_range[0], lake_index_range[1] + 1)
-            for lake_index in _lake_indices:
-                file_path = os.path.join(year_dir, files_spec.format( year=year, lake_index=lake_index ) )
-                if year == year_range[0]:
-                    if os.path.isfile( file_path ):
-                        lake_masks[lake_index] = collections.OrderedDict( )
-                        lake_masks[lake_index][year] = self.convert( file_path ) if reproject_inputs else file_path
-                        lake_indices.append( lake_index )
-                elif os.path.isfile( file_path ):
-                    lake_masks[lake_index][year]= self.convert( file_path ) if reproject_inputs else file_path
+        try:
+            year_range = self._defaults['year_range']
+            return_results = kwargs.get('return_results',False)
+            lakeMaskSpecs = self._defaults.get( "lake_masks", None )
+            data_dir = lakeMaskSpecs["basedir"]
+            lake_index_range = lakeMaskSpecs["lake_index_range"]
+            directorys_spec = lakeMaskSpecs["subdir"]
+            files_spec = lakeMaskSpecs["file"]
+            lake_masks = {}
+            lake_indices = []
+            for year in range( int(year_range[0]), int(year_range[1]) + 1 ):
+                year_dir = os.path.join( data_dir, directorys_spec.format( year=year ) )
+                _lake_indices = lake_indices if year > year_range[0] else range(lake_index_range[0], lake_index_range[1] + 1)
+                for lake_index in _lake_indices:
+                    file_path = os.path.join(year_dir, files_spec.format( year=year, lake_index=lake_index ) )
+                    if year == year_range[0]:
+                        if os.path.isfile( file_path ):
+                            lake_masks[lake_index] = collections.OrderedDict( )
+                            lake_masks[lake_index][year] = self.convert( file_path ) if reproject_inputs else file_path
+                            lake_indices.append( lake_index )
+                    elif os.path.isfile( file_path ):
+                        lake_masks[lake_index][year]= self.convert( file_path ) if reproject_inputs else file_path
 
-        nproc = kwargs.get('np', cpu_count())
-        p = Pool(processes=nproc)
-        results = p.map( partial(self.process_lake_mask, lakeMaskSpecs, kwargs ), lake_masks.items() )
-        p.close()
-        p.join()
+            nproc = kwargs.get('np', cpu_count())
+            with Pool(processes=nproc) as p:
+                results = p.imap( partial(self.process_lake_mask, lakeMaskSpecs, kwargs ), lake_masks.items(), 5 )
+
+            self.logger.info( "Processes completed- exiting.")
+        except Exception as err:
+            self.logger.error(f"Exception: {err}")
+            self.logger.error( traceback.format_exc() )
 
     def process_lake_mask(self, lakeMaskSpecs: Dict, runSpecs: Dict, lake_mask_files: Tuple[int,Dict] ):
         from .lakeExtentMapping import WaterMapGenerator
@@ -64,9 +68,9 @@ class LakeMaskProcessor:
             yearly_lake_masks.name = f"Lake {lake_index} Mask"
             nx, ny = yearly_lake_masks.shape[-1], yearly_lake_masks.shape[-2]
             waterMapGenerator = WaterMapGenerator( {'lake_index': lake_index,  **self._defaults}, logger )
-            lake_results =  waterMapGenerator.process_yearly_lake_masks( lake_index, yearly_lake_masks, **runSpecs )
+            waterMapGenerator.process_yearly_lake_masks( lake_index, yearly_lake_masks, **runSpecs )
             logger.info(f"Completed processing lake {lake_index}")
-            return  [lake_index, lake_results, yearly_lake_masks]
+            return lake_index
         except Exception as err:
             logger.error(f"Skipping lake {lake_index} due to errors ")
             logger.error( traceback.format_exc() )
