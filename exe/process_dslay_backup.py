d4a64fbc246c056c44bea2ae36d6d9df0df07f96
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-09-29 13:41:08 -0600

    Update for floodmap NRT data

--- exe/process_dslay_backup.py
+++ exe/process_dslay_backup.py
@@ -6,10 +6,11 @@ nproc = cpu_count()
 collection = 61
 archive_dir = "/att/nobackup/tpmaxwel/data/MCDWD_NRT"
 
-def process_file( archive_dir: str, collection: str, dstr: str, hdfFilepath: str ) -> int:
+def process_file( archive_dir: str, collection: str, hdfFilepath: str ) -> int:
     fName = os.path.basename(hdfFilepath)
     fdir = os.path.dirname(hdfFilepath)
-    tile = fName.split('.')[2]
+    ftoks = fName.split('.')
+    tile, dstr = ftoks[2], ftoks[1][1:]
     outpath = f"{tile}/allData/{collection}/MCDWD_L3_F2_NRT/Recent"
     result_file = f"MCDWD_L3_F2_NRT.A{dstr}.{tile}.{collection:03}.tif"
     os.makedirs(f'{archive_dir}/{outpath}', exist_ok=True)
@@ -30,7 +31,7 @@ if __name__ == '__main__':
     infiles = glob.glob( gfstr )
     print( f"Processing HDF files for [{dstr}]: '{gfstr}'" )
     print( f"Converting {len(infiles)} hdf files, saving to location: {archive_dir}:")
-    processor = partial( process_file, archive_dir, collection, dstr )
+    processor = partial( process_file, archive_dir, collection )
 
     with get_context("spawn").Pool(processes=nproc) as p:
         results = p.map( processor, infiles )
