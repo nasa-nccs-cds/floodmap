55bee79df743f4b0b3380b3d199486ba0db3c3f6
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-10-11 18:09:17 -0600

    Update output

---
+++ exe/process_bulk_results.py
@@ -0,0 +1,28 @@
+from glob import glob
+from typing import Dict, List, Tuple, Optional
+from collections import OrderedDict
+import xarray as xa
+import csv
+import numpy as np
+from floodmap.util.configuration import opSpecs
+results_dir = opSpecs.get('results_dir')
+from datetime import datetime
+import netCDF4 as nc
+
+calendar = 'standard'
+units = 'days since 1970-01-01 00:00'
+
+def get_timestamp( tstr: str, fmversion: str ) -> datetime:
+    if fmversion == "nrt": (m, d, y) = tstr.split("-")
+    elif fmversion == "legacy": (y, m, d) = tstr.split("-")
+    else: raise Exception( f"Unrecognized fmversion: {fmversion}")
+    return datetime(int(y), int(m), int(d))
+
+for fmversion in [ "legacy", "nrt" ]:
+    result_name = f"floodmap_results_{fmversion}"
+    result_file = f"{results_dir}/{result_name}.nc"
+    dset = xa.open_dataset(result_file)
+    water_area = dset.data_vars['water_area']
+    print(water_area.dims)
+    print( water_area.data[0] )
+
