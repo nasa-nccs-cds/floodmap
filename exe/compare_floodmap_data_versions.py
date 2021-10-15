8d9b7f2b4c2104b9892f5c97e817d92a6e53d4d3
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-10-15 13:44:38 -0600

    Update output

--- exe/compare_floodmap_data_versions.py
+++ exe/compare_floodmap_data_versions.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     days = [0,260]
     (xmin, xmax, ymin, ymax) = (-86.9, -86.8, 47.6, 47.7)
 
-    for day in days:
-        legacy_tile = TileLocator.get_tiles_legacy(xmin, xmax, ymin, ymax)
+    for day in range(*days):
+        legacy_tile = TileLocator.get_tiles_legacy(xmin, xmax, ymin, ymax)[0]
         legacy_data_file = f"{data_loc}/{legacy_tile}/MWP_{year}{day:03}_{legacy_tile}_2D2OT.tif"
 
-        nrt_tile = TileLocator.get_tiles(xmin, xmax, ymin, ymax)
+        nrt_tile = TileLocator.get_tiles(xmin, xmax, ymin, ymax)[0]
         nrt_data_file = f"{data_loc}/{nrt_tile}/{nrt_path}/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
 
         if not os.path.isfile(legacy_data_file): print( f"Legacy file does not exist: {legacy_data_file}" )
