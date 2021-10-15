e656cadc5a6739551b90eae238fecab60eb39f6f
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-10-15 13:38:04 -0600

    Update output

--- exe/compare_floodmap_data_versions.py
+++ exe/compare_floodmap_data_versions.py
@@ -1,19 +1,21 @@
 from floodmap.surfaceMapping.tiles import TileLocator
 import os
+from floodmap.util.configuration import opSpecs
 
-data_loc = "/att/pubrepo/ILAB/projects/Birkett/MOD44W/results"
-year = 2021
-days = [0,260]
-(xmin, xmax, ymin, ymax) = (-86.9, -86.8, 47.6, 47.7)
+if __name__ == '__main__':
+    data_loc = opSpecs.get('results_dir')
+    year = 2021
+    days = [0,260]
+    (xmin, xmax, ymin, ymax) = (-86.9, -86.8, 47.6, 47.7)
 
-for day in days:
-    legacy_tile = TileLocator.get_tiles_legacy(xmin, xmax, ymin, ymax)
-    legacy_data_file = f"{data_loc}/{legacy_tile}/MWP_{year}{day:03}_{legacy_tile}_2D2OT.tif"
+    for day in days:
+        legacy_tile = TileLocator.get_tiles_legacy(xmin, xmax, ymin, ymax)
+        legacy_data_file = f"{data_loc}/{legacy_tile}/MWP_{year}{day:03}_{legacy_tile}_2D2OT.tif"
 
-    nrt_tile = TileLocator.get_tiles(xmin, xmax, ymin, ymax)
-    nrt_data_file = f"{data_loc}/{nrt_tile}/allData/61/MCDWD_L3_F2_NRT/Recent/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
+        nrt_tile = TileLocator.get_tiles(xmin, xmax, ymin, ymax)
+        nrt_data_file = f"{data_loc}/{nrt_tile}/allData/61/MCDWD_L3_F2_NRT/Recent/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
 
-    if os.path.isfile(legacy_data_file) and os.path.isfile(nrt_data_file):
-        print( f" -------------- Day: {day} -------------------------- " )
-        print( f"Legacy: {legacy_data_file}" )
-        print( f"NRT:    {nrt_data_file}" )
+        if os.path.isfile(legacy_data_file) and os.path.isfile(nrt_data_file):
+            print( f" -------------- Day: {day} -------------------------- " )
+            print( f"Legacy: {legacy_data_file}" )
+            print( f"NRT:    {nrt_data_file}" )
