75bbe48fa3c3a698f96eadc69f8b42eb55eb36ec
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-10-15 13:39:53 -0600

    Update output

--- exe/compare_floodmap_data_versions.py
+++ exe/compare_floodmap_data_versions.py
@@ -4,6 +4,7 @@ from floodmap.util.configuration import opSpecs
 
 if __name__ == '__main__':
     data_loc = opSpecs.get('results_dir')
+    nrt_path = "allData/61/MCDWD_L3_F2_NRT/Recent"
     year = 2021
     days = [0,260]
     (xmin, xmax, ymin, ymax) = (-86.9, -86.8, 47.6, 47.7)
@@ -13,7 +14,7 @@ if __name__ == '__main__':
         legacy_data_file = f"{data_loc}/{legacy_tile}/MWP_{year}{day:03}_{legacy_tile}_2D2OT.tif"
 
         nrt_tile = TileLocator.get_tiles(xmin, xmax, ymin, ymax)
-        nrt_data_file = f"{data_loc}/{nrt_tile}/allData/61/MCDWD_L3_F2_NRT/Recent/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
+        nrt_data_file = f"{data_loc}/{nrt_tile}/{nrt_path}/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
 
         if os.path.isfile(legacy_data_file) and os.path.isfile(nrt_data_file):
             print( f" -------------- Day: {day} -------------------------- " )
