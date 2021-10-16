52a687e1abb76c15edd9a3b4183396facb47d55c
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-10-16 12:26:29 -0600

    Update output

--- exe/compare_floodmap_data_versions.py
+++ exe/compare_floodmap_data_versions.py
@@ -15,51 +15,63 @@ def get_centroid( tile: str ) -> Tuple[float,float]:
 if __name__ == '__main__':
     data_loc = opSpecs.get('results_dir')
     nrt_path = "allData/61/MCDWD_L3_F2_NRT/Recent"
-    legacy_tiles = [ os.path.basename(tpath) for tpath in glob.glob(f"{data_loc}/???[EW]???[NS]") ]
-    for tile in legacy_tiles:
-        pos = get_centroid( tile )
-        tile_test = TileLocator.get_tiles_legacy(pos[0], pos[0], pos[1], pos[1])[0]
-        nrt_tile = TileLocator.get_tiles(pos[0], pos[0], pos[1], pos[1])[0]
-        assert tile == tile_test, f"\nTile mismatch: {pos}-> {tile} = {tile_test}: {nrt_tile} \n"
-        print( f" {pos}-> {tile} = {tile_test}: {nrt_tile}" )
-
-    exit(0)
     scale = 0.00001
     year = 2021
     days = [10,260]
-    (xmin, xmax, ymin, ymax) = (-86.9, -86.8, 47.6, 47.7)
-    legacy_nodata, legacy_size = [], []
-    nrt_nodata, nrt_size = [], []
+    tot_legacy_nodata, tot_legacy_size = [], []
+    tot_nrt_nodata, tot_nrt_size = [], []
 
-    for day in range(*days):
-        legacy_tile = TileLocator.get_tiles_legacy(xmin, xmax, ymin, ymax)[0]
-        legacy_data_file = f"{data_loc}/{legacy_tile}/MWP_{year}{day:03}_{legacy_tile}_2D2OT.tif"
+    legacy_tiles = [ os.path.basename(tpath) for tpath in glob.glob(f"{data_loc}/???[EW]???[NS]") ]
+    for legacy_tile in legacy_tiles:
+        pos = get_centroid( legacy_tile )
+        nrt_tile = TileLocator.get_tiles(pos[0], pos[0], pos[1], pos[1])[0]
 
-        nrt_tile = TileLocator.get_tiles(xmin, xmax, ymin, ymax)[0]
-        nrt_data_file = f"{data_loc}/{nrt_tile}/{nrt_path}/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
+        legacy_nodata, legacy_size = [], []
+        nrt_nodata, nrt_size = [], []
 
-        if not os.path.isfile(legacy_data_file): print( f"\nLegacy file does not exist: {legacy_data_file}\n" )
-        elif not os.path.isfile(nrt_data_file):  print( f"\nNRT file does not exist: {nrt_data_file}\n" )
+        for day in range(*days):
+            legacy_data_file = f"{data_loc}/{legacy_tile}/MWP_{year}{day:03}_{legacy_tile}_2D2OT.tif"
+            nrt_data_file = f"{data_loc}/{nrt_tile}/{nrt_path}/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
+
+            if not os.path.isfile(legacy_data_file): print( f"\nLegacy file does not exist: {legacy_data_file}\n" )
+            elif not os.path.isfile(nrt_data_file):  print( f"\nNRT file does not exist: {nrt_data_file}\n" )
+            else:
+    #            print( f" -------------- Day: {day} -------------------------- " )
+                legacy_data: xa.DataArray = xa.open_rasterio(legacy_data_file).squeeze(drop=True)
+                nrt_data: xa.DataArray = xa.open_rasterio(nrt_data_file).squeeze(drop=True)
+                legacy_nodata_mask = (legacy_data == 0)
+                nrt_nodata_mask = (nrt_data == 255)
+                legacy_nt = legacy_data.size
+                legacy_nz = np.count_nonzero(legacy_nodata_mask.values)
+                nrt_nt = nrt_data.size
+                nrt_nz = np.count_nonzero(nrt_nodata_mask.values)
+                legacy_nodata.append( legacy_nz * scale )
+                legacy_size.append( legacy_nt * scale )
+                nrt_nodata.append( nrt_nz * scale )
+                nrt_size.append( nrt_nt * scale )
+        if len(legacy_size) == 0:
+            print( f"Skipping tile {legacy_tile}: NO DATA")
         else:
-#            print( f" -------------- Day: {day} -------------------------- " )
-            legacy_data: xa.DataArray = xa.open_rasterio(legacy_data_file).squeeze(drop=True)
-            nrt_data: xa.DataArray = xa.open_rasterio(nrt_data_file).squeeze(drop=True)
-            legacy_nodata_mask = (legacy_data == 0)
-            nrt_nodata_mask = (nrt_data == 255)
-            legacy_nt = legacy_data.size
-            legacy_nz = np.count_nonzero(legacy_nodata_mask.values)
-            nrt_nt = nrt_data.size
-            nrt_nz = np.count_nonzero(nrt_nodata_mask.values)
-            legacy_nodata.append( legacy_nz * scale )
-            legacy_size.append( legacy_nt * scale )
-            nrt_nodata.append( nrt_nz * scale )
-            nrt_size.append( nrt_nt * scale )
+            tile_legacy_nodata = np.array(legacy_nodata).sum()
+            tile_legacy_size = np.array(legacy_size).sum()
+            tile_nrt_nodata = np.array(nrt_nodata).sum()
+            tile_nrt_size = np.array(nrt_size).sum()
+            tile_legacy_pctn =  tile_legacy_nodata / tile_legacy_size
+            tile_nrt_pctn = tile_nrt_nodata / tile_nrt_size
+            print(f" TILE {legacy_tile}: LEGACY: {tile_legacy_pctn*100}%, NRT: {tile_nrt_pctn*100}%" )
 
-    legacy_total = np.array(legacy_nodata).sum() / np.array(legacy_size).sum()
-    nrt_total = np.array(nrt_nodata).sum() / np.array(nrt_size).sum()
+        tot_legacy_nodata.append( tile_legacy_nodata )
+        tot_legacy_size.append( tile_legacy_size )
+        tot_nrt_nodata.append( tile_nrt_nodata )
+        tot_nrt_size.append( tile_nrt_size )
 
-    print(f" LEGACY: {legacy_total*100} %")
-    print(f" NRT:    {nrt_total*100} %")
+    result_legacy_nodata = np.array(tot_legacy_nodata).sum()
+    result_legacy_size = np.array(tot_legacy_size).sum()
+    result_nrt_nodata = np.array(tot_nrt_nodata).sum()
+    result_nrt_size = np.array(tot_nrt_size).sum()
+    result_legacy_pctn = result_legacy_nodata / result_legacy_size
+    result_nrt_pctn = result_nrt_nodata / result_nrt_size
+    print(f" RESULT: LEGACY: {result_legacy_pctn * 100}%, NRT: {result_nrt_pctn * 100}%")
 
 
 
