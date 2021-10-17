25c2dd58c47c6c80c2b0cc6e93efecc33b92478d
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-10-17 15:31:01 -0600

    Update output

--- floodmap/surfaceMapping/lakeExtentMapping.py
+++ floodmap/surfaceMapping/lakeExtentMapping.py
@@ -408,15 +408,15 @@ class WaterMapGenerator(ConfigurableObject):
         results_dir = self._opspecs.get('results_dir')
         patched_water_maps_file = f"{results_dir}/lake_{lake_index}_patched_water_masks"
         result_file = patched_water_maps_file + ".tif" if format ==  'tif' else patched_water_maps_file + ".nc"
-        specs_file = f"{results_dir}/lake_{lake_index}_stats_legacy.txt"
+        specs_file = f"{results_dir}/lake_{lake_index}_stats_legacy_alt.txt"
         if skip_existing and os.path.isfile(specs_file):
             msg = f" --------------------->> Skipping already processed file: {specs_file}"
             print( msg )
             self.logger.info( msg )
             return None
         else:
-            print( f"Processing lake {lake_index}")
-            self.logger.info(f" --------------------->> Generating result file: {specs_file}")
+            print( f"Processing lake {lake_index} to result {specs_file}" )
+            self.logger.info( f" --------------------->> Generating result file: {specs_file}" )
             y_coord, x_coord = self.yearly_lake_masks.coords[ self.yearly_lake_masks.dims[-2]].values, self.yearly_lake_masks.coords[self.yearly_lake_masks.dims[-1]].values
             self.roi_bounds = [x_coord[0], x_coord[-1], y_coord[0], y_coord[-1]]
             (water_mapping_data, time_values) = self.get_mpw_data( **self._opspecs )
