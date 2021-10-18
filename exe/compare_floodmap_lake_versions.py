33c6e25d3df853ee6c2833119e0b13682fface92
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-10-18 11:08:37 -0600

    Add lake nodata test

--- exe/compare_floodmap_lake_versions.py
+++ exe/compare_floodmap_lake_versions.py
@@ -37,11 +37,12 @@ if __name__ == '__main__':
 
         for ( lake_index, lake_mask_bounds ) in lake_masks.items():
             lake_mask_specs = LakeMaskProcessor.read_lake_mask( lake_index, lake_mask_bounds )
-            waterMapGenerator.lake_mask = lake_mask_specs.get( 'mask', None )
-            (floodmap_data, time_values) = waterMapGenerator.get_mpw_data( **lake_mask_specs )
-
-            print( f"floodmap_data[{lake_index}]{floodmap_data.dims}, shape = {floodmap_data.shape} {floodmap_data.dims} ")
+            lake_mask = lake_mask_specs.get( 'mask', None )
+            locations = dataMgr.infer_tile_locations( lake_mask=lake_mask )
+            print( locations )
             exit(0)
+            pos = get_centroid(legacy_tile)
+            nrt_tile = TileLocator.get_tiles(pos[0], pos[0], pos[1], pos[1])[0]
 
             legacy_nodata, legacy_size = [], []
             nrt_nodata, nrt_size = [], []
@@ -50,10 +51,13 @@ if __name__ == '__main__':
                 legacy_data_file = f"{data_loc}/{legacy_tile}/MWP_{year}{day:03}_{legacy_tile}_2D2OT.tif"
                 nrt_data_file = f"{data_loc}/{nrt_tile}/{nrt_path}/MCDWD_L3_F2_NRT.A{year}{day:03}.{nrt_tile}.061.tif"
 
-                if not os.path.isfile(legacy_data_file): print(f" LSKIP-{day}", end=''); sys.stdout.flush()
-                elif not os.path.isfile(nrt_data_file):  print(f" NSKIP-{day}", end=''); sys.stdout.flush()
+                if not os.path.isfile(legacy_data_file):
+                    print(f" LSKIP-{day}", end=''); sys.stdout.flush()
+                elif not os.path.isfile(nrt_data_file):
+                    print(f" NSKIP-{day}", end=''); sys.stdout.flush()
                 else:
-                    print(f" {day}", end=''); sys.stdout.flush()
+                    print(f" {day}", end='');
+                    sys.stdout.flush()
                     legacy_data: xa.DataArray = xa.open_rasterio(legacy_data_file).squeeze(drop=True)
                     nrt_data: xa.DataArray = xa.open_rasterio(nrt_data_file).squeeze(drop=True)
                     legacy_nodata_mask = (legacy_data == 0)
@@ -62,35 +66,39 @@ if __name__ == '__main__':
                     legacy_nz = np.count_nonzero(legacy_nodata_mask.values)
                     nrt_nt = nrt_data.size
                     nrt_nz = np.count_nonzero(nrt_nodata_mask.values)
-                    legacy_nodata.append( legacy_nz * scale )
-                    legacy_size.append( legacy_nt * scale )
-                    nrt_nodata.append( nrt_nz * scale )
-                    nrt_size.append( nrt_nt * scale )
+                    legacy_nodata.append(legacy_nz * scale)
+                    legacy_size.append(legacy_nt * scale)
+                    nrt_nodata.append(nrt_nz * scale)
+                    nrt_size.append(nrt_nt * scale)
             if len(legacy_size) == 0:
-                print( f"\nSkipping tile {legacy_tile}: NO DATA")
+                print(f"\nSkipping tile {legacy_tile}: NO DATA")
             else:
                 tile_legacy_nodata = np.array(legacy_nodata).sum()
                 tile_legacy_size = np.array(legacy_size).sum()
                 tile_nrt_nodata = np.array(nrt_nodata).sum()
                 tile_nrt_size = np.array(nrt_size).sum()
-                tile_legacy_pctn =  tile_legacy_nodata / tile_legacy_size
+                tile_legacy_pctn = tile_legacy_nodata / tile_legacy_size
                 tile_nrt_pctn = tile_nrt_nodata / tile_nrt_size
-                print(f"\n\nTILE {legacy_tile}: LEGACY: {tile_legacy_pctn*100:.2f}%, NRT: {tile_nrt_pctn*100:.2f}%, pct_diff: {pct_diff(tile_legacy_pctn,tile_nrt_pctn):.2f}" )
-                output_file.write( f"{legacy_tile}, {tile_legacy_pctn*100:.2f}, {tile_nrt_pctn*100:.2f}, {pct_diff(tile_legacy_pctn,tile_nrt_pctn):.2f}\n" )
+                print(
+                    f"\n\nTILE {legacy_tile}: LEGACY: {tile_legacy_pctn * 100:.2f}%, NRT: {tile_nrt_pctn * 100:.2f}%, pct_diff: {pct_diff(tile_legacy_pctn, tile_nrt_pctn):.2f}")
+                output_file.write(
+                    f"{legacy_tile}, {tile_legacy_pctn * 100:.2f}, {tile_nrt_pctn * 100:.2f}, {pct_diff(tile_legacy_pctn, tile_nrt_pctn):.2f}\n")
 
-            tot_legacy_nodata.append( tile_legacy_nodata )
-            tot_legacy_size.append( tile_legacy_size )
-            tot_nrt_nodata.append( tile_nrt_nodata )
-            tot_nrt_size.append( tile_nrt_size )
+            tot_legacy_nodata.append(tile_legacy_nodata)
+            tot_legacy_size.append(tile_legacy_size)
+            tot_nrt_nodata.append(tile_nrt_nodata)
+            tot_nrt_size.append(tile_nrt_size)
 
-        result_legacy_nodata = np.array(tot_legacy_nodata).sum()
-        result_legacy_size = np.array(tot_legacy_size).sum()
-        result_nrt_nodata = np.array(tot_nrt_nodata).sum()
-        result_nrt_size = np.array(tot_nrt_size).sum()
-        result_legacy_pctn = result_legacy_nodata / result_legacy_size
-        result_nrt_pctn = result_nrt_nodata / result_nrt_size
-        print(f"\n\nRESULT: LEGACY: {result_legacy_pctn * 100:.2f}%, NRT: {result_nrt_pctn * 100:.2f}%, pct_diff: {pct_diff(result_legacy_pctn,result_nrt_pctn):.2f}")
-        output_file.write( f"TOTAL, {result_legacy_pctn * 100:.2f}, {result_nrt_pctn * 100:.2f}, {pct_diff(result_legacy_pctn, result_nrt_pctn):.2f}\n")
+    result_legacy_nodata = np.array(tot_legacy_nodata).sum()
+    result_legacy_size = np.array(tot_legacy_size).sum()
+    result_nrt_nodata = np.array(tot_nrt_nodata).sum()
+    result_nrt_size = np.array(tot_nrt_size).sum()
+    result_legacy_pctn = result_legacy_nodata / result_legacy_size
+    result_nrt_pctn = result_nrt_nodata / result_nrt_size
+    print(
+        f"\n\nRESULT: LEGACY: {result_legacy_pctn * 100:.2f}%, NRT: {result_nrt_pctn * 100:.2f}%, pct_diff: {pct_diff(result_legacy_pctn, result_nrt_pctn):.2f}")
+    output_file.write(
+        f"TOTAL, {result_legacy_pctn * 100:.2f}, {result_nrt_pctn * 100:.2f}, {pct_diff(result_legacy_pctn, result_nrt_pctn):.2f}\n")
 
 
 
