3347bd01264fe3d9b39a888ea7e08bacea44af8e
Thomas Maxwell <thomas.maxwell@nasa.gov>
2022-07-27 13:28:03 -0600

    Update processing.

--- exe/process_dslay_backup.py
+++ exe/process_dslay_backup.py
@@ -29,7 +29,7 @@ def process_file( archive_dir: str, collection: str, hdfFilepath: str ) -> int:
 if __name__ == '__main__':
     freeze_support()
     t0 = time.time()
-    dstr = f"2022*"
+    dstr = f"2021*"
     tile = "h*v*" # "h*v*" "h20v09"
     source_dir = f"/adapt/nobackup/people/dslaybac/MCDWD_NRT/MCDWD_L3_NRT_{dstr}"
     gfstr = f"{source_dir}/MCDWD_L3_NRT.A{dstr}.{tile}.{collection:03d}.*.hdf"
