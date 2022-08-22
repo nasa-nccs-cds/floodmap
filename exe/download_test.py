3a731798366986b7420e5fe99c8c17533b818871
Thomas Maxwell <thomas.maxwell@nasa.gov>
2022-08-22 11:29:17 -0600

    Fix spatial referencing

--- exe/download_test.py
+++ exe/download_test.py
@@ -12,7 +12,7 @@ product= "MCDWD_L3_F2_NRT"
 tile="h21v10"
 day = 203
 
-url = f"https://nrt4.modaps.eosdis.nasa.gov/api/v2/content/archives/{product}/allData/61//Recent/{product}.A2022{day}.{tile}.061.tif"
+url = f"https://nrt3.modaps.eosdis.nasa.gov/api/v2/content/archives/{product}/allData/61//Recent/{product}.A2022{day}.{tile}.061.tif"
 token = "dHBtYXh3ZWw6ZEdodmJXRnpMbTFoZUhkbGJHeEFibUZ6WVM1bmIzWT06MTYzMTgxNTYzOTo2ZjRjMTZjMmRiMjE1ZGZhNGIzNGYxNGQxZmQ1YWFhODg3ZWIwOTg1"
 
 download( url, token )
